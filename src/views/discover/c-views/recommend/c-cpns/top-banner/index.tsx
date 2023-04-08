9a8e2a9ff43133a4dbf8bf6aa0e990e3d827251e
linzhan <819080720@qq.com>
2023-04-08 22:16:46 +0800

    02.2 - 项目搭建 - discover 搭建 #### 轮播图结构 搭建完成

--- src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,6 +1,9 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { shallowEqualApp, useAppSelector } from '@/store'
+import { Carousel } from 'antd'
+
+import { BannerWrapper, BannerControl, BannerLeft, BannerRight } from './style'
 
 interface IProps {
     children?: ReactNode
@@ -15,11 +18,32 @@ const TopBanner: FC<IProps> = () => {
     }, shallowEqualApp)
 
     return (
-        <div>
-            {banners.map((item) => {
-                return <div key={item.imageUrl}>{item.imageUrl}</div>
-            })}
-        </div>
+        <BannerWrapper>
+            <div className="banner wrap-v2">
+                <BannerLeft>
+                    <Carousel autoplay autoplaySpeed={1500}>
+                        {banners.map((item) => {
+                            return (
+                                <div
+                                    className="banner-item"
+                                    key={item.imageUrl}
+                                >
+                                    <img
+                                        src={item.imageUrl}
+                                        alt={item.typeTitle}
+                                    />
+                                </div>
+                            )
+                        })}
+                    </Carousel>
+                </BannerLeft>
+                <BannerRight></BannerRight>
+                <BannerControl>
+                    <button className="btn left"></button>
+                    <button className="btn right"></button>
+                </BannerControl>
+            </div>
+        </BannerWrapper>
     )
 }
 
