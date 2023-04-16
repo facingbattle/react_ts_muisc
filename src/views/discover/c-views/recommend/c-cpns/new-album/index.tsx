d1a974aee09a6148b0edb4d21e9f465e1b7f4056
linzhan <819080720@qq.com>
2023-04-16 18:33:37 +0800

    02.3 - 项目搭建 - discover - 热门推荐## 新碟上架 切换按钮的事件处理

--- src/views/discover/c-views/recommend/c-cpns/new-album/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/new-album/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { ElementRef, memo, useRef } from 'react'
 import type { FC, ReactNode } from 'react'
 import { AlbumWrapper } from '@/views/discover/c-views/recommend/c-cpns/new-album/style'
 import AreaHeaderV1 from '@/components/area-header-v1'
@@ -9,14 +9,37 @@ interface IProps {
 }
 
 const NewAlbum: FC<IProps> = () => {
+    // 定义内部数据
+    const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+
+    // 事件处理函数
+    const handlePrevClick = () => {
+        bannerRef.current?.prev()
+    }
+    const handleNextClick = () => {
+        bannerRef.current?.next()
+    }
+
     return (
         <AlbumWrapper>
             <AreaHeaderV1 title="新碟上架" moreLink="/discover/album" />
             {/* 轮播图 */}
             <div className="content">
-                <button className="sprite_02 arrow arrow-left"></button>
-                <div className="banner"></div>
-                <button className="sprite_02 arrow arrow-right"></button>
+                <button
+                    className="sprite_02 arrow arrow-left"
+                    onClick={handlePrevClick}
+                ></button>
+                <div className="banner">
+                    <Carousel ref={bannerRef} dots={false} speed={1500}>
+                        {[0, 1].map((item, index) => {
+                            return <h1 key={index}>{item}</h1>
+                        })}
+                    </Carousel>
+                </div>
+                <button
+                    className="sprite_02 arrow arrow-right"
+                    onClick={handleNextClick}
+                ></button>
             </div>
         </AlbumWrapper>
     )
